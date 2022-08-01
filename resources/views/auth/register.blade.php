6954a16436cbfc27ac4e77b96bac43c1517aa4e5
Luis Sarabia <luissarabia80@gmail.com>
2022-08-01 09:24:50 -0400

    Implements Auth With API Tokens

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -25,7 +25,7 @@
             </div>
 
             <div class="mt-4">
-                <x-jet-label for="password_confirmation" value="{{ __('Confirmasr clave') }}" />
+                <x-jet-label for="password_confirmation" value="{{ __('Confirmar clave') }}" />
                 <x-jet-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
             </div>
 
