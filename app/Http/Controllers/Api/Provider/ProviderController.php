a673ff36732673e71d8918a231ea3d56ba38aac4
goyo1393 <jgescobarquintero@gmail.com>
2022-08-12 17:23:45 -0400

    Controller and Routes Provider

---
+++ app/Http/Controllers/Api/Provider/ProviderController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers\Api\Provider;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Provider;
+use Illuminate\Support\Facades\Validator;
+use Symfony\Component\HttpFoundation\Response;
+
+class ProviderController extends Controller
+{
+
+    private $rules = [
+        'category_id' => 'required',
+        'agreement_id' => 'required',
+    ];
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $provider = Provider::all();
+        return $this->showAll($provider);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validate = Validator::make($request->all(), $this->rules);
+
+        if ($validate->fails()) {
+            return $this->errorResponse($validate->errors(), Response::HTTP_BAD_REQUEST);
+        }
+        $provider = Provider::create(
+            $request->all(),
+        );
+        return $this->showOne($provider, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Request $request, Provider $provider)
+    {
+        return $this->showOne($provider);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Provider $provider)
+    {
+
+        $validate = Validator::make($request->all(), $this->rules);
+        if ($validate->fails()) {
+            return $this->errorResponse($validate->errors(), Response::HTTP_BAD_REQUEST);
+        }
+        $provider->update($request->all());
+        return $this->showOne($provider);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Provider $provider)
+    {
+        $provider->delete();
+        return $this->showOne($provider);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\Category\CategoryController;
+use App\Http\Controllers\Api\Provider\ProviderController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,6 +20,14 @@ Route::post('/category', [CategoryController::class, 'store']);
 Route::get('/category/{category}', [CategoryController::class, 'show']);
 Route::put('/category/{category}', [CategoryController::class, 'update']);
 Route::delete('/category/{category}', [CategoryController::class, 'destroy']);
+
+// Providers
+Route::get('/provider', [ProviderController::class, 'index']);
+Route::post('/provider', [ProviderController::class, 'store']);
+Route::get('/provider/{provider}', [ProviderController::class, 'show']);
+Route::post('/provider/{provider}', [ProviderController::class, 'update']);
+Route::delete('/provider/{provider}', [ProviderController::class, 'destroy']);
+//End Providers
 
 Route::post('login', [AuthController::class, 'login']);
 Route::post('signup', [AuthController::class, 'signUp']);
