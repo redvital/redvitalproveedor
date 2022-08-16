f8cf775831a86893f958d38ada7663df5988be1b
goyo1393 <jgescobarquintero@gmail.com>
2022-08-16 19:08:26 -0400

    Creation tables Stores and Stoc_products

---
+++ database/migrations/2022_08_16_230158_create_stock_products.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stock_products', function (Blueprint $table) {
+            $table->id();
+            $table->string('quantity');
+            $table->foreignId('product_id')->references('id')->on('productos')->onDelete("cascade");
+            $table->foreignId('store_id')->references('id')->on('stores')->onDelete("cascade");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stock_products');
+    }
+};
