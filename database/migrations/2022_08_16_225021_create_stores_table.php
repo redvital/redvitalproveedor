1bc22ef2f7c2294ef0228b54888a348d7c269833
goyo1393 <jgescobarquintero@gmail.com>
2022-08-16 20:29:05 -0400

    Update table stores include time stamp

--- database/migrations/2022_08_16_225021_create_stores_table.php
+++ database/migrations/2022_08_16_225021_create_stores_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->string('location');
             $table->string('description');
             $table->string('code');
+            $table->timestamps();
         });
     }
 
