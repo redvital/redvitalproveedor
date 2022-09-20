b0ba91a703873108e4c741e4a193656696b6b7bf
redvital <webmaster.redvital@gmail.com>
2022-09-20 15:38:33 -0400

    feat: update field in detail provider

--- app/Http/Controllers/Api/Provider/ProviderController.php
+++ app/Http/Controllers/Api/Provider/ProviderController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api\Provider;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Provider;
+use App\Models\SupplierBankDetails;
 use Illuminate\Support\Facades\Validator;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -18,6 +19,17 @@ class ProviderController extends Controller
         'rif' => 'required',
         'provider_type' => 'required',
     ];
+
+    private $rulesDetails = [
+        'bank' => 'required',
+        'currency' => 'required',
+        'method_of_payment' => 'required',
+        'account_type' => 'required',
+        'account_number' => 'required',
+        'account_holder' => 'required',
+        'rif' => 'required',
+    ];
+
 
     /**
      * Display a listing of the resource.
@@ -49,24 +61,35 @@ class ProviderController extends Controller
         return $this->showOne($provider, 201);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+
     public function show(Request $request, Provider $provider)
     {
         return $this->showOne($provider);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+    public function detail(Request $request, Provider $provider)
+    {
+        // $out = new \Symfony\Component\Console\Output\ConsoleOutput();
+        // $out->writeln($provider->id);
+        $validate = Validator::make($request->all(), $this->rulesDetails);
+        if ($validate->fails()) {
+            return $this->errorResponse($validate->errors(), Response::HTTP_BAD_REQUEST);
+        }
+
+        $details = new SupplierBankDetails();
+        $details->bank = $request->bank;
+        $details->currency = $request->currency;
+        $details->method_of_payment = $request->method_of_payment;
+        $details->account_type = $request->account_type;
+        $details->account_number = $request->account_number;
+        $details->account_holder = $request->account_holder;
+        $details->rif = $request->rif;
+        $details->provider = $provider->id;
+        $details->observations = $request->observations;
+        $details->save();
+        return $this->showOne($details, 201);
+    }
+
     public function update(Request $request, Provider $provider)
     {
 
@@ -78,12 +101,6 @@ class ProviderController extends Controller
         return $this->showOne($provider);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy(Provider $provider)
     {
         $provider->delete();
