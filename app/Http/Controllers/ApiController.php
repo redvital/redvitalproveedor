2055ecd168ee180b7c6f156f2f693c37db236043
kvasquez123 <kvasquez@qantamedia.com>
2022-11-08 09:26:51 -0400

    mfix: update exception Error

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -4,8 +4,13 @@ namespace App\Http\Controllers;
 
 use App\Traits\ApiResponse;
 use Illuminate\Http\Request;
+use Illuminate\Auth\Access\AuthorizationException;
 
 class ApiController extends Controller
 {
     use ApiResponse;
+    public function __construct()
+    {
+    	$this->middleware('auth:api');
+    }
 }
