07fa1e44b27e8c394f55cd64715e650d6ab84277
kvasquez123 <kvasquez@qantamedia.com>
2022-11-09 08:23:17 -0400

    fix: success in http request

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,8 +4,11 @@ namespace App\Exceptions;
 
 use Exception;
 use Throwable;
+use Symfony\Component\HttpFoundation\Response;
 use App\Traits\ApiResponse;
 use Asm89\Stack\CorsService;
+use Illuminate\Database\Events\QueryExecuted;
+use GuzzleHttp\Exception\ClientException;
 use Illuminate\Database\QueryException;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Session\TokenMismatchException;
@@ -19,24 +22,18 @@ use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
 
 class Handler extends ExceptionHandler
 {
-    use ApiResponse;
     /**
      * A list of the exception types that should not be reported.
      *
      * @var array
      */
-    protected $dontReport = [
-        //
-    ];
+    use ApiResponse;
 
-    /**
-     * A list of the inputs that are never flashed for validation exceptions.
-     *
-     * @var array
-     */
-    protected $dontFlash = [
-        'password',
-        'password_confirmation',
+    protected $dontReport = [
+        AuthorizationException::class,
+        HttpException::class,
+        ModelNotFoundException::class,
+        ValidationException::class,
     ];
 
     /**
@@ -44,10 +41,12 @@ class Handler extends ExceptionHandler
      *
      * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
      *
-     * @param  \Exception  $exception
+     * @param  \Throwable  $exception
      * @return void
+     *
+     * @throws \Exception
      */
-    public function report( Throwable $exception)
+    public function report(Throwable $exception)
     {
         parent::report($exception);
     }
@@ -56,106 +55,87 @@ class Handler extends ExceptionHandler
      * Render an exception into an HTTP response.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \Exception  $exception
-     * @return \Illuminate\Http\Response
+     * @param  \Throwable  $exception
+     * @return \Illuminate\Http\Response|\Illuminate\Http\JsonResponse
+     *
+     * @throws \Throwable
      */
     public function render($request, Throwable $exception)
     {
-        $response = $this->handleException($request, $exception);
-
-        app(CorsService::class)->addActualRequestHeaders($response, $request);
-
-        return $response;
-    }
-
-    public function handleException($request, Exception $exception)
-    {
-        if ($exception instanceof ValidationException) {
+        if($exception instanceof ValidationException)
+        {
             return $this->convertValidationExceptionToResponse($exception, $request);
         }
 
-        if ($exception instanceof ModelNotFoundException) {
+        if($exception instanceof ModelNotFoundException)
+        {
             $modelo = strtolower(class_basename($exception->getModel()));
-            return $this->errorResponse("No existe ninguna instancia de {$modelo} con el id especificado", 404);
+            return $this->errorResponse("No existe ninguna incidencia de {$modelo} con el id solicitado", 404);
         }
 
-        if ($exception instanceof AuthenticationException) {
+       /*  if($exception instanceof AuthenticationException) 
+        {
             return $this->unauthenticated($request, $exception);
+        } */
+        
+        if($exception instanceof AuthenticationException)
+        {
+            return $this->errorResponse($exception->getMessage(), RESPONSE::HTTP_UNAUTHORIZED);
+        }
+        if($exception instanceof AuthorizationException)
+        {
+            return $this->errorResponse(' No posee permiso para ejecutar esta accion', 403);
         }
 
-        if ($exception instanceof AuthorizationException) {
-            return $this->errorResponse('No posee permisos para ejecutar esta acción', 403);
+        if($exception instanceof NotFoundHttpException)
+        {
+            return $this->errorResponse('No se encontro la url especificada', 404);
         }
 
-        if ($exception instanceof NotFoundHttpException) {
-            return $this->errorResponse('No se encontró la URL especificada', 404);
+        if($exception instanceof MethodNotAllowedHttpException)
+        {
+            return $this->errorResponse('el metodo especificado no e valido', 405);
         }
-
-        if ($exception instanceof MethodNotAllowedHttpException) {
-            return $this->errorResponse('El método especificado en la petición no es válido', 405);
-        }
-
-        if ($exception instanceof HttpException) {
+        if($exception instanceof HttpException)
+        {
             return $this->errorResponse($exception->getMessage(), $exception->getStatusCode());
         }
+        if($exception instanceof ClientException)
+        {
+            $messeger = $exception->getResponse()->getBody();
+            $code = $exception->getCode();
+            return $this->errorMessage($messeger,$code);
 
-        if ($exception instanceof QueryException) {
+
+        }
+
+        if($exception instanceof QueryExecuted)
+        {
             $codigo = $exception->errorInfo[1];
-
-            if ($codigo == 1451) {
-                return $this->errorResponse('No se puede eliminar de forma permamente el recurso porque está relacionado con algún otro.', 409);
+            if($codigo == 1451)
+            {
+                return $this->errorResponse(' No se puede eliminar de forma permanete el recurso porque esta relacionado con algun otro', 409);
             }
+            
         }
 
-        if ($exception instanceof TokenMismatchException) {
-            return redirect()->back()->withInput($request->input());
+        if(config('app.debug'))
+        {
+
+            return parent::render($request, $exception);
         }
 
-        if (config('app.debug')) {
-            return parent::render($request, $exception);            
-        }
+        return $this->errorResponse('Falla inesperad, intente luego',500);
 
-        return $this->errorResponse('Falla inesperada. Intente luego', 500);
+        
     }
 
-    /**
-     * Convert an authentication exception into an unauthenticated response.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Illuminate\Auth\AuthenticationException  $exception
-     * @return \Illuminate\Http\Response
-     */
-    protected function unauthenticated($request, AuthenticationException $exception)
-    {
-        if ($this->isFrontend($request)) {
-            return redirect()->guest('login');
-        }
-        return $this->errorResponse('No autenticado.', 401);        
-    }
-
-    /**
-     * Create a response object from the given validation exception.
-     *
-     * @param  \Illuminate\Validation\ValidationException  $e
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Symfony\Component\HttpFoundation\Response
-     */
     protected function convertValidationExceptionToResponse(ValidationException $e, $request)
     {
-        $errors = $e->validator->errors()->getMessages();
-
-        if ($this->isFrontend($request)) {
-            return $request->ajax() ? response()->json($errors, 422) : redirect()
-                ->back()
-                ->withInput($request->input())
-                ->withErrors($errors);
-        }
-
-        return $this->errorResponse($errors, 422);
-    }
-
-    private function isFrontend($request)
-    {
-        return $request->acceptsHtml() && collect($request->route()->middleware())->contains('web');
+      
+        
+        $error = $e->validator->errors()->getMessages();
+        return $this->errorResponse($error, 422);
+        
     }
 }
