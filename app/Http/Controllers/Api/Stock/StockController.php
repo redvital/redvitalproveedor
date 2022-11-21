93a40715690894dd5bbffbd1eaf41a93b1c0649f
goyo1393 <jgescobarquintero@gmail.com>
2022-11-21 16:53:03 -0400

    Update

--- app/Http/Controllers/Api/Stock/StockController.php
+++ app/Http/Controllers/Api/Stock/StockController.php
@@ -25,23 +25,25 @@ class StockController extends Controller
         'product_id' => 'required'
     ];
 
-    public function index(Request $request, Stores $store_id ,Provider $supplier_id)
+    public function index(Request $request)
     {
-        $stock_data = StockResource::collection(Stock::where('store_id', $store_id->id)->where('supplier_id', $supplier_id->id)->get());
+        $query = $request->query();
+
+        if( !empty($query)){
+            list('store' => $store, 'supplier_id' => $supplier) = $query;
+            if($store  && $supplier ){
+                $stock_data = StockResource::collection(Stock::all()->where('store_id', $store)->where('supplier_id', $supplier));
+            }else if ($store > 0){
+                $stock_data = StockResource::collection(Stock::all()->where('store_id', $store));
+            }else if ($supplier > 0){
+                $stock_data = StockResource::collection(Stock::all()->where('supplier_id', $supplier));
+            }
+        } else {
+            $stock_data = StockResource::collection(Stock::all());
+        }
 
         $stock = $this->paginate($stock_data);
-
-        $provider = Provider::where('id',$request->supplier_id->id)->first();
-
-        $store = Stores::where('id', $request->store_id->id)->first();
-
-        $store_provider = [
-            'stock_data' => $stock,
-            'provider' => $provider,
-            'store' => $store
-        ];
-
-        return $store_provider;
+        return $stock;
     }
 
     /**
