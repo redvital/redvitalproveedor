ecdaa46eebfa31083973116d8b285e22724e86a0
kvasquez123 <kvasquez@qantamedia.com>
2022-11-25 18:29:18 -0400

    update provider and product

--- database/migrations/2022_08_03_215054_create_providers_table.php
+++ database/migrations/2022_08_03_215054_create_providers_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('providers', function (Blueprint $table) {
             $table->id();
-            $table->longText('name');
+            $table->string('name');
             $table->string('email');
             $table->string('phone_number')->nullable();
             $table->string('company');
